7931ee98f95cd7f00bad34204666c394021500d6
AshishCodeCrft <163163006+AshishCodeCrft@users.noreply.github.com>
2024-07-16 23:43:03 +0530

    Delete Event_Management_System directory

--- Event_Management_System/src/main/java/com/event_management/repository/FeedbackRepository.java
+++
@@ -1,8 +0,0 @@
-package com.event_management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.event_management.model.Feedback;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-   
-}
